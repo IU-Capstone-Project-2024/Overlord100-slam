c0f125a4df28fb606a8ad78b3ebc61ffbc30b01e
ekaterina <mozhegova04@gmail.com>
2024-07-14 11:51:44 +0300

    Black fix

--- src/map_publisher/map_publisher/publisher_node.py
+++ src/map_publisher/map_publisher/publisher_node.py
@@ -2,11 +2,12 @@ import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
 
+
 class MapPublisher(Node):
 
     def __init__(self):
-        super().__init__('map_publisher')
-        self.publisher_ = self.create_publisher(OccupancyGrid, '/map', 10)
+        super().__init__("map_publisher")
+        self.publisher_ = self.create_publisher(OccupancyGrid, "/map", 10)
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.publish_map)
 
@@ -15,22 +16,23 @@ class MapPublisher(Node):
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "map"
 
-        msg.info.resolution = 1.0 # The map resolution [m/cell]
-        msg.info.width = 10 # Map width [cells]
-        msg.info.height = 10 # Map height [cells]
-        
+        msg.info.resolution = 1.0  # The map resolution [m/cell]
+        msg.info.width = 10  # Map width [cells]
+        msg.info.height = 10  # Map height [cells]
+
         # Origin position
         msg.info.origin.position.x = 0.0
         msg.info.origin.position.y = 0.0
         msg.info.origin.position.z = 0.0
         # Orientation
-        msg.info.origin.orientation.w = 1.0 
+        msg.info.origin.orientation.w = 1.0
 
         # The map data, in row-major order, starting with (0,0).  Occupancy
         # probabilities are in the range [0,100].  Unknown is -1.
         msg.data = [-1] * (msg.info.width * msg.info.height)
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing an empty occupancy grid')
+        self.get_logger().info("Publishing an empty occupancy grid")
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -40,5 +42,6 @@ def main(args=None):
     map_publisher.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/map_publisher/setup.py
+++ src/map_publisher/setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'map_publisher'
+package_name = "map_publisher"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Iurii Podkorytov',
-    maintainer_email='i.podkorytov@innopolis.university',
-    description='Example of topic that publishes nav_msgs/OccupancyGrid',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Iurii Podkorytov",
+    maintainer_email="i.podkorytov@innopolis.university",
+    description="Example of topic that publishes nav_msgs/OccupancyGrid",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-                'talker = map_publisher.publisher_node:main',
+        "console_scripts": [
+            "talker = map_publisher.publisher_node:main",
         ],
     },
 )
