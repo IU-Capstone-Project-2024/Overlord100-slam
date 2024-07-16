f18d882f084f652111ccfff0ab40f0be9e9fc4fd
Iurii-Podkorytov <i.podkorytov@innopolis.university>
2024-07-16 21:13:47 +0300

    added launch file and organized the package

---
+++ overlord100_slam/launch/slam_launch.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+  # Parameters for launch files
+  mapper_params_file = LaunchConfiguration('slam_params_file', default=PathJoinSubstitution([FindPackageShare('overlord100_slam'), 'config', 'mapper_params_online_async.yaml']))
+
+  # Launch the laser scan merger
+  scan_merger_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([FindPackageShare('ros2_laser_scan_merger'), '/launch', '/merge_2_scan.launch.py']),
+  )
+
+  # Launch the slam_toolbox
+  slam_toolbox_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([FindPackageShare('slam_toolbox'), '/launch', '/online_async_launch.py']),
+    launch_arguments=[
+      ('slam_params_file', mapper_params_file)
+    ]
+  )
+
+  return LaunchDescription([
+    scan_merger_launch,
+    slam_toolbox_launch,
+  ])
